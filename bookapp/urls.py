7bb85c553651ae8ff27c81c4c6b06be5ed9cde38
Kulesh Serega <kulesh.serega@gmail.com>
2017-10-12 18:04:42 +0300

    initial commit

---
+++ bookapp/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from bookapp import views
+
+
+urlpatterns = [
+    url(r'^$', views.BookView.as_view(), name='index'),
+    url(r'^list/$', views.BooksListView.as_view(), name='book_list'),
+    url(r'^file/(?P<pk>\d+)/$', views.get_file, name='download_book'),
+]
---
+++ userapp/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from bookapp import views
+
+
+urlpatterns = [
+    url(r'^register/$', views.RegisterFormView.as_view(), name='register'),
+    url(r'^login/$', views.LoginFormView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+]
